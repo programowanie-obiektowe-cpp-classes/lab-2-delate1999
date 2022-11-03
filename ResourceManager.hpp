8a2771f392589dfd6b5fcc03ce94599f5264100d
MK <mat.kozlowski99@gmail.com>
2022-11-03 15:48:39 +0100

    V1.0

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -5,4 +5,29 @@
 class ResourceManager
 {
     // Twoja implementacja tutaj
+    public:
+        ResourceManager() : resource()   {}
+
+        ~ResourceManager(){
+            delete &resource;
+        }
+
+        double get(){
+            return resource.get();
+        }
+
+        ResourceManager(const ResourceManager& _resource_manager){
+            resource = _resource_manager.resource;
+        }
+
+        ResourceManager operator=(const ResourceManager& _resource_manager){
+            if(this != &_resource_manager){
+                resource = _resource_manager.resource;
+            }
+
+            return *this;
+        }
+
+    private:
+        Resource resource;
 };
